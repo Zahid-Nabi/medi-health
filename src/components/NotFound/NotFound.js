21252b3cb56fb1bb0ab8ffaa6638778c108b5807
Zahid-Nabi <jobaidunnabi@gmail.com>
2021-10-18 12:13:51 +0600

    Not Found Page Added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaHome } from 'react-icons/fa';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>404, page not found</h2>
+        <div className="not-found">
+            <div>
+                <h1>404</h1>
+                <h2>Oops! Page Not Found</h2>
+                <p>We are sorry! The page you request was not found.</p>
+                <div className="not-found-btn">
+                    <Link to="/home"><button className="go-home-btn"><FaHome /> <span>Go Home</span></button></Link>
+                </div>
+            </div>
         </div>
     );
 };
